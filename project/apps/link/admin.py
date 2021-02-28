3dd2c11f8d09a051ce19fddbd0bc03ffe3d36940
QuantumaStelata <quantumastelata@gmail.com>
2021-02-28 10:47:40 +0300

    Heroku Django Bot

--- project/apps/link/admin.py
+++ project/apps/link/admin.py
@@ -7,4 +7,12 @@ class LinkAdmin(admin.ModelAdmin):
     list_display = ('email', 'token', 'number_visits', 'active')
 
 import requests
-requests.get('https://quantumas.herokuapp.com')
+import threading
+import time
+
+def main():
+    while True:
+        requests.get('https://quantumas.herokuapp.com')
+        time.sleep(600)
+
+threading.Thread(target=main, args=()).start()
